5de10c406091f8d980873f83f5fff4dac311cf42
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-17 20:44:51 +0100

    setup component filestructure add added PhotoGrid component to App

---
+++ src/components/PhotoGrid.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PhotoGrid() {
+    return (
+        <div className='photo-grid'>
+            <p>Placeholder Text</p>
+        </div>
+    )
+}
+
+export default PhotoGrid
