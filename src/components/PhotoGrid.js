ff78f080b9cc4878a694392789c02f9454d753af
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-17 21:10:31 +0100

    added axios via terminal

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PhotoCard from "./PhotoCard.js"
+import axios from "axios"
 
 function PhotoGrid() {
+
+    const [photos, setPhotos] = useState([]);
+    // const date = '2012-03-14'
+
+    useEffect(() => {
+        axios
+            .get(`https://api.nasa.gov/planetary/apod?api_key=89ntUtLrjYyw6hhSA6e7fKp31fVfzFUGHuDTkmCl&date=2012-03-14`)
+            
+            .then(response => {
+                const nasaData = response.data
+            })
+    
+    }, [])
+
+    console.log(nasaData)
     return (
         <div className='photo-grid'>
             <p>Placeholder Text</p>
