dde816e3e960929a8d1dee46711d0cc09a41c0d2
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-17 21:40:29 +0100

    added functionality to call nasa api for specified dates

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -5,19 +5,35 @@ import axios from "axios"
 function PhotoGrid() {
 
     const [photos, setPhotos] = useState([]);
-    // const date = '2012-03-14'
+    const dates = ['2012-03-14', '2012-03-13', '2012-03-11']
+
+    
+    // useEffect(() => {
+    //     axios
+    //         .get(`https://api.nasa.gov/planetary/apod?api_key=89ntUtLrjYyw6hhSA6e7fKp31fVfzFUGHuDTkmCl&date=2012-03-14`)
+            
+    //         .then(response => {
+    //             console.log(response.data)
+    //             // const nasaData = response.data
+    //         })
+    
+    // }, [])
 
     useEffect(() => {
-        axios
-            .get(`https://api.nasa.gov/planetary/apod?api_key=89ntUtLrjYyw6hhSA6e7fKp31fVfzFUGHuDTkmCl&date=2012-03-14`)
-            
-            .then(response => {
-                console.log(response.data)
-                // const nasaData = response.data
-            })
-    
-    }, [])
 
+        dates.forEach(function(date) {
+            axios
+            .get(`https://api.nasa.gov/planetary/apod?api_key=89ntUtLrjYyw6hhSA6e7fKp31fVfzFUGHuDTkmCl&date=${date}`)
+            .then(response => {
+                photos.push(response.data)
+                console.log(photos)
+            })
+            
+        
+        })
+        
+    }, [])
+    
     // console.log(nasaData)
     return (
         <div className='photo-grid'>
